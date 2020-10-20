94b334a2441bfd4d11c03813c59b9230290caece
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-10-20 16:48:31 -0300

    Se agregó la solución al ejercicio 3 tp3

---
+++ unidad_3/mpi/ejercicio_3/ejercicio_3_sawczuk.cpp
@@ -0,0 +1,58 @@
+#include "mpi.h"
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char *argv[])
+{
+    int rank, buzon, size;
+    MPI_Status estado;
+ 
+    MPI_Init(&argc, &argv); // Inicializamos la comunicacion de los procesos
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtenemos el valor de nuestro identificador
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &size); //Guarda en size la cantidad de procesos que hay en e comunicador
+ 
+    //Envia y recibe mensajes
+    if(rank == 0){
+        MPI_Send(
+        &rank ,
+        1, 
+        MPI_INT, // Tipo de dato que envias
+        rank+1, // pid del proceso destino
+        0, //etiqueta
+        MPI_COMM_WORLD
+      ); 
+    }else{
+      cout << rank << " - antes de recibir:" << buzon << endl;
+
+      MPI_Recv(&buzon // Referencia al vector donde se almacenara lo recibido
+        ,1 // tamaño del vector a recibir
+        ,MPI_INT // Tipo de dato que recibe
+        ,rank-1 // pid del proceso origen de la que se recibe
+        ,0 // etiqueta
+        ,MPI_COMM_WORLD // Comunicador por el que se recibe
+        ,&estado); // estructura informativa del estado
+
+      cout<< "Soy el proceso "<<rank<<" y he recibido "<<buzon<<endl;
+
+      if (rank < size-1){
+        MPI_Send(
+            &rank ,
+            1, 
+            MPI_INT, // Tipo de dato que envias
+            rank+1, // pid del proceso destino
+            0, //etiqueta
+            MPI_COMM_WORLD
+        ); 
+      }
+
+ 
+    }
+ 
+	
+    MPI_Finalize();
+    
+
+    return 0;
+}
