cb97587a3efe6cfb33a4b3e2508529d95e4094b2
Oscar <kaitosaw@gmail.com>
2020-10-20 22:55:37 -0300

    Se agrega comentarios al codigo

--- unidad_3/mpi/ejercicio_2/ejercicio_2_zarske.cpp
+++ unidad_3/mpi/ejercicio_2/ejercicio_2_zarske.cpp
@@ -4,12 +4,13 @@ using namespace std;
  
 int main(int argc, char *argv[])
 {
-    int rank, size;
+    int rank;
  
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv); //se inicia la comunicación paralela entre procesos
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); //se obtiene en "rank" el identificador del proceso segun el comunicador MPI_COMM_WORLD
     
+    //segun el numero que tenga como identificador el proceso, se analiza si es par o impar; y segun esto imprime un mensaje.
     if(rank % 2 == 0){
 
         printf("Soy el proceso:  %d, y soy un proceso Par \n", rank);
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 
     }
  
-    MPI_Finalize();
+    MPI_Finalize(); //se finaliza la comunicación paralela entre procesos
     
     return 0;
 }
