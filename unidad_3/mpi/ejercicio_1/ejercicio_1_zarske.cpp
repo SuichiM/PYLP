4c78e056bbc7f4f160b87b08ec8472c7633b2288
Oscar <kaitosaw@gmail.com>
2020-10-20 22:32:43 -0300

    Se agrega comentarios al codigo

--- unidad_3/mpi/ejercicio_1/ejercicio_1_zarske.cpp
+++ unidad_3/mpi/ejercicio_1/ejercicio_1_zarske.cpp
@@ -6,14 +6,14 @@ int main(int argc, char *argv[])
 {
     int rank, size;
  
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);  //se inicia la comunicación paralela entre procesos
 
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size); //se obtiene en "size" el rango del comunicador global que es MPI_COMM_WORLD
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); //se obtiene en "rank" el tamaño del comunicador MPI_COMM_WORLD
  
     printf("¡Hola Mundo desde el proceso %d de %d que somos!\n", rank, size);
  
-    MPI_Finalize();
+    MPI_Finalize();//se finaliza la comunicación paralela entre procesos
     
     return 0;
 }
