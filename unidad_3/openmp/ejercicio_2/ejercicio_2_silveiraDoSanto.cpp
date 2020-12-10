74eb4a3492e426e2dce673a8c97544dcb0312222
facundoSilveira <facundosilveiradosanto@gmail.com>
2020-12-10 18:58:42 -0300

    Adicion de correcciones de CR

--- unidad_3/openmp/ejercicio_2/ejercicio_2_silveiraDoSanto.cpp
+++ unidad_3/openmp/ejercicio_2/ejercicio_2_silveiraDoSanto.cpp
@@ -13,8 +13,10 @@ int main (){
   #pragma omp parallel private(thread)
   {
       thread = omp_get_thread_num();
-      #pragma omp for schedule(dynamic, 5) // el static divide la cantidad de datos segun el argumento pasado ordenado por los hilos
-                                          // el dynamic divide los datos de forma aleatoria para cada hilo
+      #pragma omp for schedule(dynamic, 2) // el static divide la cantidad de datos segun el argumento pasado ordenado por los hilos
+      //#pragma omp for schedule(static, 2)
+      //#pragma omp for schedule(guided, 2)
+      //#pragma omp for schedule(auto)                                    // el dynamic divide los datos de forma aleatoria para cada hilo
       for (int i = 0 ; i < N; i++){
         #pragma omp critical
           std::cout<<"Soy el proceso "<<thread<<" ejecuto al iteracion "<<i<<std::endl;
